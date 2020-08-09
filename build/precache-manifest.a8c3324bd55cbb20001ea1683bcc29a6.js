18cfc215e3d8b49dcf74b0077657c22c419222ee
Al.exe <al_exe@ukr.net>
2020-08-09 18:11:39 +0300

    build

---
+++ build/precache-manifest.a8c3324bd55cbb20001ea1683bcc29a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c2a2a534e78a2039a5bfbe04e5b0b7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc66fc69164031377e77",
+    "url": "/static/css/main.8437bfe4.chunk.css"
+  },
+  {
+    "revision": "50abd4db9e4e1dc1a56b",
+    "url": "/static/js/2.d00c5d17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d00c5d17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc66fc69164031377e77",
+    "url": "/static/js/main.9fe39cb0.chunk.js"
+  },
+  {
+    "revision": "d92a0fc4555433bebf6b",
+    "url": "/static/js/runtime-main.1c8a7f80.js"
+  }
+]);
